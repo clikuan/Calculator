d93d88b2c528443711429fcf7071aebe5c0b0b67
chen li kuan <clikuan.cs05g@g2.nctu.edu.tw>
2017-08-26 14:23:31 +0800

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by 陳立寬 on 2017/8/26.
+//  Copyright © 2017年 陳立寬. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Calculator/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by 陳立寬 on 2017/8/26.
+//  Copyright © 2017年 陳立寬. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
